baf245c248a43cf5ce6f1465dc543a15c74accc6
2023JoshuaP <jphilco@unsa.edu.pe>
2024-07-31 18:31:35 -0500

    Reiniciando el repositorio como estaba antes para corregir

--- app/shared/interfaces/documents.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Documents {
-    category: string;
-    description: string;
-    id: number;
-    image: string;
-    price: number;
-    rating: { rate: number; count: number };
-    title: string;
-}
